e37a0dd2281c612b241837d69841495177988f22
Dennis Wu <67745938+Hibb-bb@users.noreply.github.com>
2023-05-06 12:37:47 -0500

    Update wmt17.sh

--- wmt17.sh
+++ wmt17.sh
@@ -1,7 +1,7 @@
 cd docs/source/examples
 bash wmt17/prepare_wmt_ende_data.sh
 python3 ../../../onmt/bin/build_vocab.py --config wmt17/wmt17_ende2.yaml --n_sample -1
-python3 ../../../onmt/bin/train.py --config wmt17/wmt17_ende2.yaml --world_size 3 --gpu_ranks 0 1 2
+python3 ../../../onmt/bin/train.py --config wmt17/wmt17_ende2.yaml --world_size 2 --gpu_ranks 0 1
 
 python3 ../../../onmt/bin/translate.py --src wmt17_en_de/test.src.bpe --model wmt17_en_de/sparse-wmt17_step_50000.pt --beam_size 5 --batch_size 4096 --batch_type tokens --output wmt17_en_de/pred.trg.bpe --gpu 0
 sed -re 's/@@( |$)//g' < wmt17_en_de/pred.trg.bpe > wmt17_en_de/pred.trg.tok
